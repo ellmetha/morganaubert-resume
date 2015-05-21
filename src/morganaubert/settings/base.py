eb7a52fc12d6a4052d21ea4bed73e3fc3dcbfb8e
ellmetha <morgan.aubert@zoho.com>
2015-05-22 01:23:07 +0200

    Base settings updated

--- src/morganaubert/settings/base.py
+++ src/morganaubert/settings/base.py
@@ -143,8 +143,10 @@ SECRET_KEY = get_secret('SECRET_KEY')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
