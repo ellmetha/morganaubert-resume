7a004afd1e6c4d1dd77369fdb73398f2c923f6ba
Morgan Aubert <morgan.aubert@zoho.com>
2017-04-21 21:30:52 -0400

    Updated settings modules

--- ma_project/settings/base.py
+++ ma_project/settings/base.py
@@ -5,6 +5,7 @@
     For more information on this file, see https://docs.djangoproject.com/en/dev/topics/settings/
     For the full list of settings and their values, see
     https://docs.djangoproject.com/en/dev/ref/settings/
+
 """
 
 import json
