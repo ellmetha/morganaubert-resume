1b802898b7d98cae421a763f8ceb5a97130c0a5f
Morgan Aubert <morgan.aubert@zoho.com>
2017-04-24 23:32:15 -0400

    Updated manage module

--- manage.py
+++ manage.py
@@ -3,6 +3,6 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ma_project.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ma_project.settings.base')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
